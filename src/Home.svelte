3307386226261ed4c79ea31aff1852c738e87608
Jonas Westerlund <jonas.westerlund@helsinki.fi>
2020-01-14 18:28:51 +0200

    init

---
+++ src/Home.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { getClient, query } from "svelte-apollo";
+  import gql from "graphql-tag";
+
+  const GET_QUESTIONS = gql`
+  query {
+  allQuestions {
+    id
+  }
+}`
+const client = getClient();
+let response = query(client, {query: GET_QUESTIONS});
+
+console.log(response);
+</script>
