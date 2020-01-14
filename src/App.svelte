bb7891c1ada570771bb78a77a86b34dee7ad4b66
Jonas Westerlund <jonas.westerlund@helsinki.fi>
2020-01-14 21:20:01 +0200

    Testing GraphQL-queries

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,36 @@
 <script>
-    import { Router, Link, Route } from "svelte-routing";
-    import Home from './Home.svelte';
-	  import ApolloClient from "apollo-boost";
+  import { Router, Link, Route } from "svelte-routing";
+  import Home from "./Home.svelte";
+  import ApolloClient from "apollo-boost";
   import { setClient } from "svelte-apollo";
 
-  const client = new ApolloClient({ uri: "http://localhost:3001/graphql"});
+  const client = new ApolloClient({ uri: "http://localhost:3001/graphql" });
   setClient(client);
 
-  let route = 'home';
+  let route = "home";
 </script>
 
 <style>
-    .tabs {
-      margin-top: 20px;
-    }
-  </style>
-  
-  <svelte:head>
-      <title>MathDQ</title>
-    </svelte:head>
-    
-    <Router>
-      <div class="tabs is-centered is-boxed">
-        <ul>
-        <li class:is-active="{route === 'home'}"><Link to="/" on:click={() => (route = 'home')}>Home</Link></li>
-      </ul>
-    </div>
-      <div>
-        <Route path="/"><Home /></Route>
-      </div>
-    </Router>
+  .tabs {
+    margin-top: 20px;
+  }
+</style>
+
+<svelte:head>
+  <title>MathDQ</title>
+</svelte:head>
+
+<Router>
+<!--   <div class="tabs is-centered is-boxed">
+    <ul>
+      <li class:is-active={route === 'home'}>
+        <Link to="/" on:click={() => (route = 'home')}>Home</Link>
+      </li>
+    </ul>
+  </div> -->
+  <div>
+    <Route path="/">
+      <Home />
+    </Route>
+  </div>
+</Router>
--- src/Home.svelte
+++ src/Home.svelte
@@ -2,14 +2,36 @@
   import { getClient, query } from "svelte-apollo";
   import gql from "graphql-tag";
 
-  const GET_QUESTIONS = gql`
-  query {
-  allQuestions {
-    id
-  }
-}`
-const client = getClient();
-let response = query(client, {query: GET_QUESTIONS});
+  const GET_QUESTION = gql`
+    query($id: ID!) {
+      question(id: $id) {
+        id
+      }
+    }
+  `;
+  const client = getClient();
 
-console.log(response);
+  let question = query(client, {
+    query: GET_QUESTION,
+    variables: { id: "fee11f33-5bd2-40b7-aa68-f35ad36fa358" }
+  })
+  let input;
+  const fetchQuestion = id => {
+    console.log(id);
+    question = query(client, { query: GET_QUESTION, variables: { id } });
+  };
 </script>
+
+<section class="hero is-link is-fullheight">
+  <div class="hero-body">
+    <div class="container">
+      {#await $question then result}
+        <p>{result.data.question.id}</p>
+      {/await}
+      <input class="input" type="text" bind:value={input} />
+      <button class="button is-primary" on:click={() => fetchQuestion(input)}>
+        Random question
+      </button>
+    </div>
+  </div>
+</section>
