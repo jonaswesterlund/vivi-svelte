8ca0428b500465664906cad4cca037f1a2617070
Jonas Westerlund <jonas.westerlund@helsinki.fi>
2021-08-06 21:36:04 +0300

    Removed unnecessary toggle edit button

--- src/pages/Edit.svelte
+++ src/pages/Edit.svelte
@@ -1,18 +1,9 @@
 <script>
   import AddNewQuestion from '../components/AddNewQuestion.svelte';
   import AddNewCategory from '../components/AddNewCategory.svelte';
-
-  let showEdit = false;
 </script>
 
-<div>
-  <button class="button" on:click={() => (showEdit = !showEdit)}>
-    Toggle edit
-  </button>
-  {#if showEdit}
-    <div class="flex justify-between">
-      <AddNewQuestion />
-      <AddNewCategory />
-    </div>
-  {/if}
+<div class="flex justify-between">
+  <AddNewQuestion />
+  <AddNewCategory />
 </div>
